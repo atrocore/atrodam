54e77108df869d7375925e9ce844e48cd31c306f
r.ratsun <r.ratsun@gmail.com>
2020-12-16 17:55:05 +0200

    .

---
+++ app/Migrations/V1Dot1Dot2.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ *  This file is part of AtroDAM.
+ *
+ *  AtroDAM - Open Source DAM application.
+ *  Copyright (C) 2020 AtroCore UG (haftungsbeschränkt).
+ *  Website: https://atrodam.com
+ *
+ *  AtroDAM is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  AtroDAM is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with AtroDAM. If not, see http://www.gnu.org/licenses/.
+ *
+ *  The interactive user interfaces in modified source and object code versions
+ *  of this program must display Appropriate Legal Notices, as required under
+ *  Section 5 of the GNU General Public License version 3.
+ *
+ *  In accordance with Section 7(b) of the GNU General Public License version 3,
+ *  these Appropriate Legal Notices must retain the display of the "AtroDAM" word.
+ */
+
+declare(strict_types=1);
+
+namespace Dam\Migrations;
+
+use Treo\Core\Migration\Base;
+
+/**
+ * Class V1Dot1Dot2
+ */
+class V1Dot1Dot2 extends Base
+{
+    /**
+     * @inheritDoc
+     */
+    public function up(): void
+    {
+        $this->execute("ALTER TABLE `asset_type` DROP nature");
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function down(): void
+    {
+        $this->execute("ALTER TABLE `asset_type` ADD nature VARCHAR(255) DEFAULT 'File' COLLATE utf8mb4_unicode_ci");
+    }
+
+    /**
+     * @param string $sql
+     */
+    protected function execute(string $sql)
+    {
+        try {
+            $this->getPDO()->exec($sql);
+        } catch (\Throwable $e) {
+            // ignore all
+        }
+    }
+}
