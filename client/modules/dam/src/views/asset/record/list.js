7559ddf74662d1b6e0cdeaa9927886716e2c403d
r.ratsun <r.ratsun@gmail.com>
2021-01-19 13:17:50 +0200

    .

--- client/modules/dam/src/views/asset/record/list.js
+++ client/modules/dam/src/views/asset/record/list.js
@@ -28,6 +28,95 @@
 
 Espo.define('dam:views/asset/record/list', 'dam:views/record/list',
     Dep => Dep.extend({
+
+        setup() {
+            Dep.prototype.setup.call(this);
+
+            // refresh after edit
+            this.listenTo(this, 'after:quickEditSave', function () {
+                if (this.options.panel) {
+                    this.options.panel.actionRefresh();
+                }
+            });
+        },
+
+        actionQuickEdit: function (data) {
+            data = data || {}
+            var id = data.id;
+            if (!id) return;
+
+            var model = null;
+            if (this.collection) {
+                model = this.collection.get(id);
+            }
+            if (!data.scope && !model) {
+                return;
+            }
+
+            var scope = data.scope || model.name || this.scope;
+
+            var viewName = this.getMetadata().get('clientDefs.' + scope + '.modalViews.edit') || 'views/modals/edit';
+
+            if (!this.quickEditDisabled) {
+                Espo.Ui.notify(this.translate('loading', 'messages'));
+                var options = {
+                    scope: scope,
+                    id: id,
+                    model: model,
+                    fullFormDisabled: data.noFullForm,
+                    returnUrl: this.getRouter().getCurrentUrl(),
+                    returnDispatchParams: {
+                        controller: scope,
+                        action: null,
+                        options: {
+                            isReturn: true
+                        }
+                    }
+                };
+                if (this.options.keepCurrentRootUrl) {
+                    options.rootUrl = this.getRouter().getCurrentUrl();
+                }
+                this.createView('modal', viewName, options, function (view) {
+                    view.once('after:render', function () {
+                        Espo.Ui.notify(false);
+                    });
+
+                    view.render();
+
+                    this.listenToOnce(view, 'remove', function () {
+                        this.clearView('modal');
+                    }, this);
+
+                    this.listenToOnce(view, 'after:save', function (m) {
+                        var model = this.collection.get(m.id);
+                        if (model) {
+                            model.set(m.getClonedAttributes());
+                        }
+
+                        this.trigger('after:quickEditSave', m);
+                    }, this);
+                }, this);
+            } else {
+                var options = {
+                    id: id,
+                    model: this.collection.get(id),
+                    returnUrl: this.getRouter().getCurrentUrl(),
+                    returnDispatchParams: {
+                        controller: scope,
+                        action: null,
+                        options: {
+                            isReturn: true
+                        }
+                    }
+                };
+                if (this.options.keepCurrentRootUrl) {
+                    options.rootUrl = this.getRouter().getCurrentUrl();
+                }
+                this.getRouter().navigate('#' + scope + '/edit/' + id, {trigger: false});
+                this.getRouter().dispatch(scope, 'edit', options);
+            }
+        },
+
         _getHeaderDefs() {
 
             for (var i in this.listLayout) {
