bda478da24419a8e8387374f199c4c88f715694e
r.ratsun <r.ratsun@gmail.com>
2021-01-20 18:30:46 +0200

    bug fix

--- client/modules/dam/src/views/asset/fields/file.js
+++ client/modules/dam/src/views/asset/fields/file.js
@@ -29,14 +29,6 @@
 Espo.define('dam:views/asset/fields/file', 'dam:views/fields/file',
     Dep => Dep.extend({
 
-        getDownloadUrl(id) {
-            var url = this.getBasePath() + '?entryPoint=download&showInline=false&id=' + id;
-            if (this.getUser().get('portalId')) {
-                url += '&portalId=' + this.getUser().get('portalId');
-            }
-            return url;
-        },
-
         setup() {
             Dep.prototype.setup.call(this);
 
