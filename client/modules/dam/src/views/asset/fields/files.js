8df5100b8055505036039928ce0b470bb1df1d41
r.ratsun <r.ratsun@gmail.com>
2021-02-02 10:45:26 +0200

    mass upload of assets

--- client/modules/dam/src/views/asset/fields/files.js
+++ client/modules/dam/src/views/asset/fields/files.js
@@ -36,7 +36,7 @@ Espo.define('dam:views/asset/fields/files', ['views/fields/attachment-multiple',
 
         events: _.extend(Dep.prototype.events, {
                 'click a.remove-attachment': function (e) {
-                    var $div = $(e.currentTarget).parent();
+                    let $div = $(e.currentTarget).parent();
                     let id = $div.attr('data-id');
                     if (id) {
                         $.ajax({
@@ -69,6 +69,14 @@ Espo.define('dam:views/asset/fields/files', ['views/fields/attachment-multiple',
             return File.prototype.getMaxUploadSize.call(this);
         },
 
+        slice: function (file, start, end) {
+            return File.prototype.slice.call(this, file, start, end);
+        },
+
+        createFilePieces: function (file, sliceSize, start, stream) {
+            return File.prototype.createFilePieces.call(this, file, sliceSize, start, stream);
+        },
+
         addFileBox: function (file, number) {
             let $attachments = this.$attachments;
             let removeLink = '<a href="javascript:" class="remove-attachment pull-right"><span class="fas fa-times"></span></a>';
@@ -88,8 +96,6 @@ Espo.define('dam:views/asset/fields/files', ['views/fields/attachment-multiple',
         },
 
         uploadFiles: function (files) {
-            const maxFileSize = this.getMaxUploadSize();
-
             let fileList = [];
             let attachmentBoxes = [];
             for (let i = 0; i < files.length; i++) {
@@ -124,9 +130,14 @@ Espo.define('dam:views/asset/fields/files', ['views/fields/attachment-multiple',
             let file = files.shift();
             let $attachmentBox = attachmentBoxes.shift();
 
-            if (this.ignoredNumbers.indexOf(this.currentNumber) !== -1) {
+            if (this.isCanceled()) {
                 this.uploadedCount++;
                 this.afterAttachmentUploaded(files, attachmentBoxes);
+                return;
+            }
+
+            if (file.size > this.getMaxUploadSize() * 1024 * 1024) {
+                this.chunkCreateAttachments(file, $attachmentBox, files, attachmentBoxes);
                 return;
             }
 
@@ -146,28 +157,152 @@ Espo.define('dam:views/asset/fields/files', ['views/fields/attachment-multiple',
                         field: this.name,
                         modelAttributes: this.model.attributes
                     }),
-                }).done(function (attachment) {
-                    $attachmentBox.parent().find('.uploading-message').remove();
-                    $attachmentBox.attr('data-id', attachment.id);
-
-                    this.uploadedFiles[attachment.id] = attachment.name;
-
-                    this.uploadedCount++;
-
-                    this.afterAttachmentUploaded(files, attachmentBoxes);
+                }).done(function (response) {
+                    this.uploadSuccess(response, $attachmentBox, files, attachmentBoxes);
                 }.bind(this)).error(function (response) {
-                    let reason = response.getResponseHeader('X-Status-Reason') || this.translate('Failed');
-
-                    $attachmentBox.parent().find('.uploading-message').html(reason);
-                    $attachmentBox.css('background-color', '#f2dede');
-
-                    this.totalCount--;
-                    this.failedCount++;
-
-                    this.afterAttachmentUploaded(files, attachmentBoxes);
+                    this.uploadFailed(response, $attachmentBox, files, attachmentBoxes);
                 }.bind(this));
             }.bind(this);
             fileReader.readAsDataURL(file);
+        },
+
+        chunkCreateAttachments: function (file, $attachmentBox, files, attachmentBoxes) {
+            const chunkId = File.prototype.createChunkId.call(this);
+            const chunkFileSize = this.getConfig().get('chunkFileSize') || 2;
+            const sliceSize = chunkFileSize * 1024 * 1024;
+
+            this.streams = this.getConfig().get('fileUploadStreamCount') || 3;
+            this.piecesTotal = Math.ceil(file.size / sliceSize);
+            this.pieceNumber = 0;
+
+            this.setProgressMessage($attachmentBox);
+
+            // create file pieces
+            this.pieces = [];
+            this.createFilePieces(file, sliceSize, 0, 1);
+
+            let promiseList = [];
+            promiseList.push(new Promise(function (resolve) {
+                let stream = 1;
+                while (stream <= this.streams) {
+                    let pieces = [];
+                    this.pieces.forEach(function (row) {
+                        if (row.stream === stream) {
+                            pieces.push(row);
+                        }
+                    });
+                    this.sendChunk(resolve, file, pieces, chunkId, $attachmentBox, files, attachmentBoxes);
+                    stream++;
+                }
+            }.bind(this)));
+
+            Promise.all(promiseList).then(function () {
+                this.createByChunks(file, chunkId, $attachmentBox, files, attachmentBoxes);
+            }.bind(this));
+
+        },
+
+        isCanceled: function () {
+            return this.ignoredNumbers.indexOf(this.currentNumber) !== -1;
+        },
+
+        sendChunk: function (resolve, file, pieces, chunkId, $attachmentBox, files, attachmentBoxes) {
+            if (pieces.length === 0 || this.isCanceled()) {
+                resolve();
+                return;
+            }
+
+            const item = pieces.shift();
+
+            const reader = new FileReader();
+            reader.readAsDataURL(item.piece);
+
+            reader.onloadend = function () {
+                $.ajax({
+                    type: 'POST',
+                    url: 'Attachment/action/CreateChunks',
+                    contentType: "application/json",
+                    data: JSON.stringify({
+                        chunkId: chunkId,
+                        start: item.start,
+                        piece: reader.result,
+                    }),
+                }).done(function (data) {
+                    this.pieceNumber++;
+                    this.setProgressMessage($attachmentBox);
+
+                    if (pieces.length > 0) {
+                        this.sendChunk(resolve, file, pieces, chunkId, $attachmentBox, files, attachmentBoxes);
+                    }
+
+                    if (this.pieceNumber === this.piecesTotal) {
+                        resolve();
+                    }
+                }.bind(this)).error(function (response) {
+                    this.uploadFailed(response, $attachmentBox, files, attachmentBoxes);
+                    resolve();
+                }.bind(this));
+            }.bind(this)
+        },
+
+        createByChunks: function (file, chunkId, $attachmentBox, files, attachmentBoxes) {
+            if (this.pieces.length === 0 || this.isCanceled()) {
+                this.uploadSuccess(null, $attachmentBox, files, attachmentBoxes);
+                return;
+            }
+
+            $.ajax({
+                type: 'POST',
+                url: 'Attachment/action/CreateByChunks',
+                contentType: "application/json",
+                data: JSON.stringify({
+                    chunkId: chunkId,
+                    name: file.name,
+                    type: file.type || 'text/plain',
+                    size: file.size,
+                    role: 'Attachment',
+                    relatedType: this.model.name,
+                    field: this.name,
+                    modelAttributes: this.model.attributes
+                }),
+            }).done(function (response) {
+                this.pieces = [];
+                this.uploadSuccess(response, $attachmentBox, files, attachmentBoxes);
+            }.bind(this)).error(function (response) {
+                this.uploadFailed(response, $attachmentBox, files, attachmentBoxes);
+            }.bind(this));
+        },
+
+        setProgressMessage: function ($attachmentBox) {
+            let total = this.piecesTotal + 1;
+            let percent = this.pieceNumber / total * 100;
+
+            $attachmentBox.parent().find('.uploading-message').html(this.translate('Uploading...') + ' <span class="uploading-progress-message">' + percent.toFixed(0) + '%</span>');
+        },
+
+        uploadSuccess: function (attachment, $attachmentBox, files, attachmentBoxes) {
+            $attachmentBox.parent().find('.uploading-message').remove();
+
+            if (attachment !== null) {
+                $attachmentBox.attr('data-id', attachment.id);
+                this.uploadedFiles[attachment.id] = attachment.name;
+            }
+
+            this.uploadedCount++;
+
+            this.afterAttachmentUploaded(files, attachmentBoxes);
+        },
+
+        uploadFailed: function (response, $attachmentBox, files, attachmentBoxes) {
+            let reason = response.getResponseHeader('X-Status-Reason') || this.translate('Failed');
+
+            $attachmentBox.parent().find('.uploading-message').html(reason);
+            $attachmentBox.css('background-color', '#f2dede');
+
+            this.totalCount--;
+            this.failedCount++;
+
+            this.afterAttachmentUploaded(files, attachmentBoxes);
         },
 
         afterAttachmentUploaded(files, attachmentBoxes) {
