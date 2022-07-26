2d9333b1c08a23f388ff91db5160b6c541657e22
r.ratsun <r.ratsun@gmail.com>
2022-07-26 11:29:09 +0300

    .

--- app/Services/AssetType.php
+++ app/Services/AssetType.php
@@ -33,9 +33,56 @@ declare(strict_types=1);
 
 namespace Dam\Services;
 
-/**
- * Class AssetType
- */
-class AssetType extends \Espo\Core\Templates\Services\Base
+use Espo\Core\EventManager\Event;
+use Espo\Core\Templates\Services\Base;
+
+class AssetType extends Base
 {
+    public function massRemove(array $params)
+    {
+        $params = $this->dispatchEvent('beforeMassRemove', new Event(['params' => $params]))->getArgument('params');
+
+        $ids = [];
+        if (array_key_exists('ids', $params) && !empty($params['ids']) && is_array($params['ids'])) {
+            $ids = $params['ids'];
+        }
+
+        if (array_key_exists('where', $params)) {
+            $selectParams = $this->getSelectParams(['where' => $params['where']]);
+            $this->getEntityManager()->getRepository($this->getEntityType())->handleSelectParams($selectParams);
+
+            $query = $this
+                ->getEntityManager()
+                ->getQuery()
+                ->createSelectQuery($this->getEntityType(), array_merge($selectParams, ['select' => ['id']]));
+
+            $ids = $this
+                ->getEntityManager()
+                ->getPDO()
+                ->query($query)
+                ->fetchAll(\PDO::FETCH_COLUMN);
+        }
+
+        $inTransaction = false;
+        if (!$this->getEntityManager()->getPDO()->inTransaction()) {
+            $this->getEntityManager()->getPDO()->beginTransaction();
+            $inTransaction = true;
+        }
+
+        try {
+            foreach ($ids as $id) {
+                $this->deleteEntity($id);
+            }
+            if ($inTransaction) {
+                $this->getEntityManager()->getPDO()->commit();
+            }
+        } catch (\Throwable $e) {
+            if ($inTransaction) {
+                $this->getEntityManager()->getPDO()->rollBack();
+            }
+            throw $e;
+        }
+
+        return $this->dispatchEvent('afterMassRemove', new Event(['result' => ['count' => count($ids), 'ids' => $ids]]))->getArgument('result');
+    }
 }
