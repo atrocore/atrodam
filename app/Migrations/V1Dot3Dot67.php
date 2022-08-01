bb34a45b3a30c06c4fe65ea8ffd7b66daf06ee4b
r.ratsun <r.ratsun@gmail.com>
2022-08-01 15:48:18 +0300

    .

---
+++ app/Migrations/V1Dot3Dot67.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ *  This file is part of AtroDAM.
+ *
+ *  AtroDAM - Open Source DAM application.
+ *  Copyright (C) 2020 AtroCore UG (haftungsbeschränkt).
+ *  Website: https://atrodam.com
+ *
+ *  AtroDAM is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  AtroDAM is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with AtroDAM. If not, see http://www.gnu.org/licenses/.
+ *
+ *  The interactive user interfaces in modified source and object code versions
+ *  of this program must display Appropriate Legal Notices, as required under
+ *  Section 5 of the GNU General Public License version 3.
+ *
+ *  In accordance with Section 7(b) of the GNU General Public License version 3,
+ *  these Appropriate Legal Notices must retain the display of the "AtroDAM" word.
+ *
+ *  This software is not allowed to be used in Russia and Belarus.
+ */
+
+declare(strict_types=1);
+
+namespace Dam\Migrations;
+
+use Espo\Core\Exceptions\Error;
+use Treo\Core\Migration\Base;
+
+class V1Dot3Dot67 extends Base
+{
+    public function up(): void
+    {
+        $this->execute("UPDATE `asset_type` SET assign_automatically=1 WHERE `name`='File' AND deleted=0");
+    }
+
+    public function down(): void
+    {
+    }
+
+    protected function execute(string $query): void
+    {
+        try {
+            $this->getPDO()->exec($query);
+        } catch (\Throwable $e) {
+        }
+    }
+}
