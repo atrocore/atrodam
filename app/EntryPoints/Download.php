e04c06383bf86132d35af0e4a61d9305c83f4a4f
r.ratsun <r.ratsun@gmail.com>
2022-08-30 10:55:05 +0300

    .

--- app/EntryPoints/Download.php
+++ app/EntryPoints/Download.php
@@ -34,124 +34,43 @@ declare(strict_types=1);
 namespace Dam\EntryPoints;
 
 use Dam\Core\Download\Custom;
+use Espo\Entities\Attachment;
 use Espo\Core\Exceptions\BadRequest;
 use Espo\Core\Exceptions\Forbidden;
 use Espo\Core\Exceptions\NotFound;
-use Imagick;
 
-/**
- * Class Download
- * @package Dam\EntryPoints
- */
 class Download extends \Espo\EntryPoints\Download
 {
-    /**@var Imagick * */
-    protected $image = null;
-
-    /**
-     * Run download method
-     */
     public function run()
     {
         if (!empty($_GET['type']) && $_GET['type'] === 'custom') {
             $this->custom();
-        } else {
-            $this->runDownload();
         }
+
+        parent::run();
     }
 
-    /**
-     * @throws BadRequest
-     * @throws Forbidden
-     * @throws NotFound
-     * @throws \Espo\Core\Exceptions\Error
-     */
-    public function runDownload()
+    protected function custom(): void
     {
-        if (empty($_GET['id'])) {
-            throw new BadRequest();
-        }
-        $id = $_GET['id'];
-
-        $attachment = $this->getEntityManager()->getEntity('Attachment', $id);
-
-        if (!$attachment) {
-            throw new NotFound();
-        }
-
-        if (!$this->getAcl()->checkEntity($attachment)) {
-            throw new Forbidden();
-        }
-
-        $fileName = $this->getEntityManager()->getRepository('Attachment')->getFilePath($attachment);
-
-        if (!file_exists($fileName)) {
-            throw new NotFound();
-        }
-
-        header("Location: $fileName", true, 302);
-        exit;
-    }
-
-    /**
-     * @throws \ImagickException
-     */
-    protected function custom()
-    {
-        $attachment = $this->getAttachment();
-
-        $filePath = $this->getEntityManager()->getRepository('Attachment')->getFilePath($attachment);
-
-        if (!file_exists($filePath)) {
-            throw new NotFound();
-        }
-
-        $file = (new Custom($filePath))
-            ->setAttachment($attachment)
+        $converter = $this
+            ->getContainer()
+            ->get(Custom::class)
+            ->setAttachment($this->getAttachment())
             ->setParams($_GET)
             ->convert();
 
-        $type = $file->getType();
-
-        header('Content-Description: File Transfer');
-        if ($type) {
-            header('Content-Type: ' . $type);
-        }
-        header("Content-Disposition: attachment;filename=\"" . $file->getName() . "\"");
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate');
-        header('Pragma: public');
-        header('Content-Length: ' . $file->getImageSize());
-
-        echo $file->getImage();
+        header("Location: " . $converter->getFilePath(), true, 302);
         exit;
     }
 
-    /**
-     * @return bool
-     */
-    protected function showInline(): bool
-    {
-        if (!isset($_GET['showInline'])) {
-            return true;
-        }
-
-        return $_GET['showInline'] == 'true' ? true : false;
-    }
-
-    /**
-     * @return mixed
-     */
-    protected function getAttachment()
+    protected function getAttachment(): Attachment
     {
         if (empty($_GET['id'])) {
             throw new BadRequest();
         }
-        $id = $_GET['id'];
 
-        $attachment = $this->getEntityManager()->getEntity('Attachment', $id);
-
-        if (!$attachment) {
+        $attachment = $this->getEntityManager()->getEntity('Attachment', $_GET['id']);
+        if (empty($attachment)) {
             throw new NotFound();
         }
 
@@ -161,5 +80,4 @@ class Download extends \Espo\EntryPoints\Download
 
         return $attachment;
     }
-
 }
